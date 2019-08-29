39713d12e1686156e35607a4b5cf29efe3217169
Eric Bréchemier <twilio@eric.brechemier.name>
2019-08-29 22:45:19 +0200

    Split changes for Conference Funlet
    This is a partial revert of commit 0cf5475802151c5df53d3a3dfe2bea496d24b013.

---
+++ funlet-conference/tests/funlet-conference.test.js
@@ -0,0 +1,19 @@
+const funlet = require('../functions/funlet-conference');
+const runtime = require('../../test/test-helper');
+const Twilio = require('twilio');
+
+beforeAll( () =>
+  runtime.setup()
+);
+
+test.skip('Missing Tests', done => {
+  const callback = (err, result) => {
+    expect(result).toBe('...');
+    done();
+  };
+  funlet.handler({}, {}, callback);
+});
+
+afterAll( () =>
+  runtime.teardown()
+);
