03433fa42d36864fb6e00c36e2f9b4621ec7bf96
Eric Bréchemier <twilio@eric.brechemier.name>
2019-08-29 22:44:32 +0200

    Split changes for Hold Music Funlet
    This is a partial revert of commit 0cf5475802151c5df53d3a3dfe2bea496d24b013.

---
+++ funlet-hold-music/tests/funlet-hold-music.test.js
@@ -0,0 +1,19 @@
+const funlet = require('../functions/funlet-hold-music');
+const runtime = require('../../test/test-helper');
+const Twilio = require('twilio');
+
+beforeAll( () =>
+  runtime.setup()
+);
+
+test.skip('Missing Tests', done => {
+  const callback = (err, result) => {
+    expect(result).toBe('...');
+    done();
+  };
+  funlet.handler({}, {}, callback);
+});
+
+afterAll( () =>
+  runtime.teardown()
+);
